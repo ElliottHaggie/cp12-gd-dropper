8a3beb9c7aceb920d0bdf3258e85e1e0447ec3f7
CF-401-Super-Starfighter <1469130@sd61learn.ca>
2025-12-01 13:35:29 -0800

    Fixed Score system.
    it now updates as intended and will decrease score appropriate to gained score.

--- Apple game/apple-game/Global.gd
+++ Apple game/apple-game/Global.gd
@@ -4,13 +4,15 @@ var score = 0
 var killpoints = 0
 
 
-func _Process():
+func _process(delta):
 	if score >= (2 + killpoints) * killpoints:
 		killpoints += 1
 		print(killpoints)
 
 func Score_Lost():
 	score -= (1 + killpoints)
+	get_node("/root/Node2D/Panel/Score").text = "Score: " + str(score)
 
 func Score_Gained():
 	score += 1
+	get_node("/root/Node2D/Panel/Score").text = "Score: " + str(score)
