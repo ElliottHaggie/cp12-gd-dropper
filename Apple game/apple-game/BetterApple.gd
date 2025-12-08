5b1cf861f6c99f83bf5635d5b6a797be8755020b
CF-401-Super-Starfighter <1469130@sd61learn.ca>
2025-12-08 13:20:31 -0800

    Feat: dynamic apple speed
    added script to increase apple speed after every drop.

--- Apple game/apple-game/BetterApple.gd
+++ Apple game/apple-game/BetterApple.gd
@@ -2,26 +2,26 @@ extends CharacterBody2D
 
 signal lost
 var velocity_max = 200
+@onready var apple = get_node("/root/Node2D/Apple")
 
 func _ready():
 	velocity = Vector2(0, 120)
 	position.x = randi_range(52, get_viewport().size.x - 52)
-	
-	
+
+
 func silly():
-		position.y = -10
-		position.x = randi_range(52, get_viewport().size.x - 52)
-		velocity.y = velocity_max
-		velocity_max += 1
-	
-	
+	position.y = -10
+	position.x = randi_range(52, get_viewport().size.x - 52)
+	velocity_max += 1
+	velocity.y = velocity_max
+
 func _physics_process(delta: float) -> void:
-	velocity.y += 0.5
 	move_and_slide()
 	
 	if position.y > get_viewport().size.y + 20:
 		lost.emit()
 		silly()
+		apple.hide()
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	Global.Score_Gained()
