84e0bb1e0ec811b25380512a7137d978085efcd1
Elliott Haggie <ebhaggie@gmail.com>
2025-12-09 14:31:13 -0800

    feat: win screen

--- Apple game/apple-game/Global.gd
+++ Apple game/apple-game/Global.gd
@@ -1,7 +1,11 @@
 extends Node
 
 var score = 0
+var wingate = randi_range(5, 15)
 
 func Score_Gained():
 	score += 1
 	get_node("/root/Node2D/Panel/Score").text = "Score: " + str(score)
+	if (score >= wingate):
+		get_node("/root/Node2D/win screen").show()
+		get_tree().paused = true
